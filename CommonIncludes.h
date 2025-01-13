b98bda2881560a7248e49ace95aaa4c64130f905
Eduardo Pinho <enet4mikeenet@gmail.com>
2025-01-13 10:36:02 +0000

    Provide portable definition for strcasecmp and stricmp

--- CommonIncludes.h
+++ CommonIncludes.h
@@ -33,7 +33,14 @@
 
 #include <fcntl.h>
 
+#ifdef _MSC_VER 
+#define strncasecmp _strnicmp
+#define strcasecmp _stricmp
+#endif
+
+#ifndef stricmp
 #define stricmp			strcasecmp
+#endif
 #define AdPlug_LogWrite(...)		if (global_debug) printf(__VA_ARGS__)
 #define AdPlug_LogError(...)		fprintf(stderr, __VA_ARGS__)
 
